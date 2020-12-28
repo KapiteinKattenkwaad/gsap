739c90e4081fbb21a9b860c46903c77672432493
Max St <maxstouten@hotmail.com>
2020-12-28 17:12:41 +0100

    toggle actions

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ gsap.to(".square", {
         start: "top 30%",
         end: () => `+=${document.querySelector('.square').offsetHeight}`, //this will get the end value dynamically from square element, even if its set to vh or something
         toggleClass: 'red',
+        toggleActions: "restart pause resume complete",
+        //all the values u can use: play pause resume reverse restart reset complete none
+        //are being used for these actions: onEnter, onLeave, onEnterBack, onLeaveBack
         markers: true
     }
 })
