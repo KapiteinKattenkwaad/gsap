3effc029a03daff78d3c45c1429694c108bebb63
Max St <maxstouten@hotmail.com>
2020-12-28 19:18:16 +0100

    timelining

--- app.js
+++ app.js
@@ -6,15 +6,41 @@ gsap.to(".square", {
     scale: .6,
     duration: 1,
     scrollTrigger: {
-        trigger: ".square2",
-        start: "top 80%",
-        end: "top 30%",
+        trigger: ".square",
+        start: "top 20%",
+        end: "top 50%",
         pin: ".square", //you can set this to true or give it the name of the value that needs to be pinned
         pinSpacing: true,
         scrub: 4, //if you make this a number, it smoothes it out
         toggleActions: "restart none none none",
         markers: true
     }
+})
+
+const tl = gsap.timeline({
+    scrollTrigger: {
+
+        trigger: ".box",
+        start: "top 80%",
+        end: "top 30%",
+        scrub: 3, //if you make this a number, it smoothes it out
+        markers: true
+    }
+});
+
+tl
+.to(".box", {
+    x:500, 
+    duration: 5
+})
+.to(".box", {
+    y: 300,
+    duration: 2
+})
+.to(".box", {
+    x: 0,
+    y: 0,
+    duration: 3
 })
 
 
