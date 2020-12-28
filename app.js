eb4105cf9a6a2d9f4dfaa248f01faa9ff0a6bfb0
Max St <maxstouten@hotmail.com>
2020-12-28 17:02:56 +0100

    start end and markers

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ gsap.registerPlugin(ScrollTrigger)
 gsap.to(".square", {
     x: 300,
     duration: 1,
-    scrollTrigger: ".square2"
+    scrollTrigger: {
+        trigger: ".square",
+        start: "top 30%",
+        end: () => `+=${document.querySelector('.square').offsetHeight}`, //this will get the end value dynamically from square element, even if its set to vh or something
+        toggleClass: 'red',
+        markers: true
+    }
 })
 
 
