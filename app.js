71916a66cbfc3b050e18553dd501555afe4598c1
Max St <maxstouten@hotmail.com>
2020-12-28 17:43:01 +0100

    scrubbing

--- app.js
+++ app.js
@@ -6,27 +6,18 @@ gsap.to(".square", {
     scale: .6,
     duration: 1,
     scrollTrigger: {
-        trigger: ".square",
+        trigger: ".square2",
         start: "top 80%",
         end: "top 30%",
-        scrub: true, //if you make this a number, it smoothes it out
+        pin: ".square", //you can set this to true or give it the name of the value that needs to be pinned
+        pinSpacing: true,
+        scrub: 4, //if you make this a number, it smoothes it out
         toggleActions: "restart none none none",
         markers: true
     }
 })
 
-gsap.to(".title", {
-    x: 300,
-    duration: .7,
-    scrollTrigger: {
-        trigger: ".title",
-        start: "top 80%",
-        end: "top 30%",
-        scrub: 4,
-        toggleActions: "restart none none none",
-    
-    }
-})
+
 
 
 console.log('fuckkkk')
